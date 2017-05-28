5e309d4b9493be558224d8fb43ed3527c50e5a17
littleLemming <lemming0061@gmail.com>
2017-05-28 22:56:00 +0200

    structure for server, second signal handler

--- procdb-server.c
+++ procdb-server.c
@@ -17,12 +17,17 @@
  /**
  * @brief Name of the program
  */
-static const char *progname = "server"; /* default name */
+static const char *progname = "procdb-server"; /* default name */
 
  /** 
- * @brief variable that gets set as soon as a signal gets received
+ * @brief variable that gets set as soon as a SIGINT or SIGTERM signal gets received
  */
 volatile sig_atomic_t quit = 0;
+
+ /** 
+ * @brief variable that gets set as soon as a SIGUSR1 signal gets received
+ */
+volatile sig_atomic_t print_db = 0;
 
 
  /**
@@ -51,6 +56,12 @@ static void parse_args(int argc, char **argv);
  */
 static void signal_quit_handler(int sig);
 
+/**
+ * @brief Signal handler for SIGUSR1 which should print all data in the database
+ * @param sig Signal number catched
+ */
+static void signal_print_db_handler(int sig);
+
 
 static void bail_out(int exitcode, const char *fmt, ...) {
     va_list ap;
@@ -71,6 +82,7 @@ static void bail_out(int exitcode, const char *fmt, ...) {
 }
 
 static void free_resources(void) {
+    printf("freeing resources\n");
 }
 
 static void parse_args(int argc, char **argv) {
@@ -86,6 +98,11 @@ static void signal_quit_handler(int sig) {
     quit = 1;
 }
 
+static void signal_print_db_handler(int sig) {
+    print_db = 1;
+}
+
+
 /**
  * main
  * @brief starting point of program
@@ -95,22 +112,51 @@ static void signal_quit_handler(int sig) {
 int main(int argc, char *argv[]) {
 
     /* setup signal handlers */
-    const int signals[] = {SIGINT, SIGTERM};
-    struct sigaction s;
+    const int quit_signals[] = {SIGINT, SIGTERM};
+    struct sigaction s_q;
 
-    s.sa_handler = signal_quit_handler;
-    s.sa_flags   = 0;
-    if(sigfillset(&s.sa_mask) < 0) {
-        bail_out(EXIT_FAILURE, "sigfillset");
+    const int printdb_signals[] = {SIGUSR1};
+    struct sigaction s_p;
+
+    s_q.sa_handler = signal_quit_handler;
+    s_q.sa_flags   = 0;
+    if(sigfillset(&s_q.sa_mask) < 0) {
+        bail_out(EXIT_FAILURE, "sigfillset - quit");
     }
-    for(int i = 0; i < COUNT_OF(signals); i++) {
-        if (sigaction(signals[i], &s, NULL) < 0) {
-            bail_out(EXIT_FAILURE, "sigaction");
+    for(int i = 0; i < COUNT_OF(quit_signals); i++) {
+        if (sigaction(quit_signals[i], &s_q, NULL) < 0) {
+            bail_out(EXIT_FAILURE, "sigaction - quit");
+        }
+    }
+
+    s_p.sa_handler = signal_quit_handler;
+    s_p.sa_flags   = 0;
+    if(sigfillset(&s_p.sa_mask) < 0) {
+        bail_out(EXIT_FAILURE, "sigfillset - print");
+    }
+    for(int i = 0; i < COUNT_OF(printdb_signals); i++) {
+        if (sigaction(printdb_signals[i], &s_p, NULL) < 0) {
+            bail_out(EXIT_FAILURE, "sigaction - print");
         }
     }
 
     /* parse arguments */
     parse_args(argc, argv);
 
+    /* setup shared memory & semaphores */
+
+    /* wait for requests of clients, and write back answers */
+    while (TRUE) {
+        if (quit == 1) {
+            printf("caught signal - shutting down\n");
+            break;
+        }
+        if (print_db == 1) {
+            printf("printing database:\n");
+            print_db = 0;
+        }
+    }
+
+    free_resources();
     return 0;
 }
